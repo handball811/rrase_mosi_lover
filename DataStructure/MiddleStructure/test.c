a38d74c398ad1145e3c7a839acdda711cd0fb451
Airone_Cenerino <tomopc723@yahoo.co.jp>
2019-12-06 01:53:47 +0900

    MiddleStructureの大幅変更(多分こっちの方が楽) InputStructureのtest.cを変更。

--- DataStructure/MiddleStructure/test.c
+++ DataStructure/MiddleStructure/test.c
@@ -8,29 +8,17 @@ int main()
 
 	strcpy(middle.str, "abcdabcdabcdxxcd");
 	middle.str_len = 16;
-	/*
-	nを偶数とした時、
-	range[n] <= x < range[n+1] の範囲が
-	まだ確定していないところ
-	*/
-	int predict_input[] = {1,4,12,16};
-	memcpy(middle.predict, predict_input, sizeof(int)*4);
-	input.predict_len = 4;
-	/*
-	上の場合
-	1~4文字目(４文字目そのものは含まない)   bcd
-	と
-	12〜16文字目の　　　　　　　　　　　　　 xxcd
-	が確定していない
-	*/
+	
+	// メモリ確保
+	middle.parts = (char **)malloc(sizeof(char *) * 2);
+	for(int i=0;i<2;i++){
+		middle.parts[i] = (char *)malloc(sizeof(char) * 10);
+	}
 
-	strcpy(input.parts, "babcdcd");
-	int range_input[] = {0,1,3,5,7};
-	memcpy(input.range, range_input, sizeof(int)*5);
+	middle.parts[0] = "aabc";
+	middle.parts[1] = "bcd";
+	middle.partsNum = 2;
 
-	input.range_len = 5;
-	/*
-	上の部分はinput_structureと一緒！
-	*/
+	printf("%s\n", middle.str);
 	return 0;
 }
