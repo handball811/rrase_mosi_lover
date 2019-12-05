47ad15e2dc77ca18cac116433d2ef06457f281f4
Airone_Cenerino <tomopc723@yahoo.co.jp>
2019-12-06 01:52:39 +0900

    コメント追加

--- Generator/ReadDataFile.c
+++ Generator/ReadDataFile.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include "ReadDataFile.h"
 
+// ファイルを開いて返す。
 FILE* OpenFile(char* fileName){
     FILE *fp;
     if ((fp = fopen(fileName, "r")) == NULL) {
@@ -12,16 +13,17 @@ FILE* OpenFile(char* fileName){
     return fp;
 }
 
+// ファイルを読み込んでデータ構造体に格納していく。
 void ReadFile(FILE* fp, input_structure* inputStructure){
     fscanf( fp , "%s" , inputStructure->str);
     inputStructure->strLen = strlen(inputStructure->str);
 
     inputStructure->parts = (char **)malloc(400010 * sizeof(char *));
 
-    char str[10000];
+    char str[10000];    // 部分文字列を一時的に格納しておくためのもの。
     int i = 0;
     while(fscanf( fp , "%s" , str) != EOF ){
-        inputStructure->parts[i] = (char *)malloc((strlen(str)+1) * sizeof(char));
+        inputStructure->parts[i] = (char *)malloc((strlen(str)+1) * sizeof(char));  // 部分文字列の長さ分のメモリを確保。
         strcpy(inputStructure->parts[i], str);
         i++;
     }
@@ -29,10 +31,8 @@ void ReadFile(FILE* fp, input_structure* inputStructure){
     inputStructure->partsNum = i;
 }
 
-
-// 引数で受け取ったファイル名のファイルを開き、データ構造体に格納
-void GetStructureFromFile(char* fileName, input_structure* inputStructure)
-{
+// 引数で受け取ったファイル名のファイルの内容をデータ構造体に格納する。
+void GetStructureFromFile(char* fileName, input_structure* inputStructure){
     FILE *fp = OpenFile(fileName);
     ReadFile(fp, inputStructure);
     fclose(fp);
