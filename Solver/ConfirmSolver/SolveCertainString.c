6e65e4a20ff54bd29c0b5021d4ff226be4de77ee
Airone_Cenerino <tomopc723@yahoo.co.jp>
2019-12-06 11:29:17 +0900

    確実に定まるものだけ当てはめるVerだが、72%しか埋まらない。ここからはVer1を用いてうまいこと当てはめていくしかない。

--- Solver/ConfirmSolver/SolveCertainString.c
+++ Solver/ConfirmSolver/SolveCertainString.c
@@ -13,39 +13,41 @@ void SortParts(input_structure* inputStructure) {
     // BubbleSort(inputStructure->parts, inputStructure->partsNum);
 }
 
-// strにtargetが当てはまる場所を探す
-int InsertWord(char* str, int strLen, char* target, int targetLen, int phase) {
+// strにtargetが当てはまる場所が1箇所に定まればそこに入れる。
+int InsertWord(char* str, int strLen, char* target, int targetLen) {
     int i = targetLen - 1;
+    int AlreadyInsertable = 0;
+    int insertPosition = 0;
 
     while(i < strLen) { // 1文字ずつずらしていく。
         int matchCharaNum = 0;
+        int insertable = 1;    // 当てはまるかどうかのフラグ
 
-        for(int j = 0; j < targetLen;
-            j++) { // 部分文字列全体を見て、何文字完全一致しているかの確認。
-            if(target[targetLen - j - 1] == str[i - j]) // 完全一致。
-                matchCharaNum++;
-            else if(str[i - j] !=
-                    'x') { // 完全一致でもxでもないなら100%当てはまらない。
-                matchCharaNum = 0;
+        for(int j = 0; j < targetLen;j++) { // 部分文字列全体を見て当てはめることが可能か判断。
+            if(target[targetLen - j - 1] != str[i - j] && (str[i - j] !='x')){   // 完全一致でもなくxでもない時は絶対に当てはまらない。
+                insertable = 0;
                 break;
             }
         }
 
-        if(matchCharaNum >= FIT_MINIMUM_MATCH_NUM[phase]) { // 当てはめる。
-            for(int j = 0; j < targetLen; j++) {
-                if(str[i - j] == 'x') {
-                    str[i - j] = target[targetLen - j - 1];
-                }
-            }
-            // printf("insert!\n");
-            return 1;
+        if(insertable) { // 当てはめることができる
+            if(AlreadyInsertable) return 0;
+            AlreadyInsertable = 1;
+            insertPosition = i;
         }
-
         i++;
     }
 
-    // printf("not insert.\n");
-    return 0; // 当てはまらなかった。
+    if(AlreadyInsertable){  // 挿入箇所が1箇所に絞れた時
+        for(int j = 0; j < targetLen; j++) {
+            if(str[insertPosition - j] == 'x') {
+                str[insertPosition - j] = target[targetLen - j - 1];
+            }
+        }
+        return 1;
+    }
+
+    return 0; // 挿入箇所が1つもなかった時。
 }
 
 // inputStructureにある部分文字列をmiddleStructureのstrに当てはめていく。
@@ -56,29 +58,25 @@ void FitWords(input_structure *inputStructure, middle_predict_structure *middleS
         flg[i] = 0;
     }
 
-    for(int phase = 0; phase < PHASE_NUM; phase++) {
-        // 大きいものから順番に当てはめていく
-        while(1) {
-            int insertNum = 0; //この周回で何個の単語が入ったかを記録。
+    // 大きいものから順番に当てはめていく
+    while(1) {
+        int insertNum = 0; //この周回で何個の単語が入ったかを記録。
 
-            for(int i = 0; i < inputStructure->partsNum; i++) {
-                if(flg[i] || strlen(inputStructure->parts[i]) < MATCH_CHARACTER_NUM[phase])
-                    continue; // すでにその部分文字列が当てはまってた時かこのフェーズで当てはめる文字数以下のとき
+        for(int i = 0; i < inputStructure->partsNum; i++) {
+            if(flg[i])
+                continue; // すでにその部分文字列が当てはまってた時かこのフェーズで当てはめる文字数以下のとき
 
-                if(InsertWord(middleStructure->str, inputStructure->strLen,
-                              inputStructure->parts[i],
-                              strlen(inputStructure->parts[i]), phase)) { // 入れてみる
-                    flg[i] = 1; // 入ったらフラグを立てる。
-                    insertNum++;
-                }
-
-                printf("%d\n", i);
+            if(InsertWord(middleStructure->str, inputStructure->strLen, inputStructure->parts[i], strlen(inputStructure->parts[i]))) { // 入れてみる
+                flg[i] = 1; // 入ったらフラグを立てる。
+                insertNum++;
             }
-
-            // 一つも挿入出来なかったら終わり。　
-            if(insertNum == 0)
-                break;
         }
+
+        // 一つも挿入出来なかったら終わり。　
+        if(insertNum == 0)
+            break;
+
+        printf("一巡したよ\n");
     }
 
     // 最後にflg[i]==0の部分文字列をmiddleStructure->parts[]に加える。partsNumの更新も忘れずに。降順に出来れば理想だが、昇順でもいいかもね。
