06f2d2bf69448abe7419c6073e24e6fc5b52ce5d
Airone_Cenerino <tomopc723@yahoo.co.jp>
2019-12-07 21:50:56 +0900

    Solve部分完成

--- Solver/ConfirmSolver/SolveCertainString.c
+++ Solver/ConfirmSolver/SolveCertainString.c
@@ -3,21 +3,26 @@
 #include <string.h>
 #include "sort.h"
 #include "SolveCertainString.h"
-#include "InitializeMiddleStructure.h"
+#include "MiddleStructureHandler.h"
 #include "InsertConfirmParts.h"
-#include "../../Settings/confirm_solver_setting.h"
+#include "InsertPartsRoughly.h"
 
 
 // inputStructureの部分文字列を降順にソートする。
-void SortParts(input_structure* inputStructure) {
+void SortInputStructureParts(input_structure* inputStructure) {
     quicksort(inputStructure->parts, 0, inputStructure->partsNum - 1);
-    // BubbleSort(inputStructure->parts, inputStructure->partsNum);
 }
 
-
 void SolveCertainString(input_structure *inputStructure, middle_predict_structure *middleStructure) {
-    SortParts(inputStructure);                                  //まず文字数でソート
+    int *usedPartsFlg; // 部分文字列が当てはまったかどうかを管理する配列。
+    usedPartsFlg = (int *)malloc(sizeof(int) * inputStructure->partsNum);
+    for(int i = 0; i < inputStructure->partsNum; i++) {
+        usedPartsFlg[i] = 0;
+    }
+
+    SortInputStructureParts(inputStructure);   
     InitializeMiddleStructure(inputStructure, middleStructure); // inputStructureの中身を元にmiddleStructureを初期化する。移したinputStructureの短い部分文字列も消す。
-    InsertConfirmParts(inputStructure, middleStructure);        // まず確実に当てはまる部分文字列を当てはめる。
-    InsertPartsRoughly(inputStructure, middleStructure);        // 残りは一致する文字数が大きいところに挿入。
+    InsertConfirmParts(inputStructure, middleStructure, usedPartsFlg);        // まず確実に当てはまる部分文字列を当てはめる。
+    InsertPartsRoughly(inputStructure, middleStructure, usedPartsFlg);        // 残りは一致する文字数が大きいところに挿入。
+    PushUnusedPartsToMiddleStructure(inputStructure, middleStructure, usedPartsFlg);
 }
