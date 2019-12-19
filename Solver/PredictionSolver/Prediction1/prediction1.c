03b2a5d82fc623f7fdf2110eaf7aa45bd032c691
鈴木　嘉恵 <>
2019-12-19 15:57:46 +0900

    no message

--- Solver/PredictionSolver/Prediction1/prediction1.c
+++ Solver/PredictionSolver/Prediction1/prediction1.c
@@ -5,54 +5,51 @@
 middle_predict_structure prediction1(middle_predict_structure* middle){
     int partlength=0; 
     //printf("%s\n",middle->str);
-    int j=0,i=0,k=0,l=0;
-    int memo=0;
-    int flag=0;//確定か確定じゃないか判断
-
-    while(middle->unsettledPlace[i+1]!=0){//決まっていない部分がなくなるまで繰り返す
-        int wordlength=middle->unsettledPlace[i+1]-middle->unsettledPlace[i];//決まっていない単語の長さ
-        if(wordlength>2){//2文字以下の単語はここでは決まらないと考える
-            for(j=0;j<middle->partsNum[wordlength];j++){
-                for(k=0;k<wordlength;k++){
-                    if(middle->parts[wordlength][j][k]!=middle->unsettledPlace[i+wordlength]){
+    int j=0,i=0;
+    int cnt=0;//どれぐらい埋めたかカウントする
+    while(partlength!=(middle->partsNum)){//次に探すものがなくなるまで繰り返す
+        int memostr=0;//当てはまる場所のメモ用
+        int flag=0; //既に見つけてるかを判断するflag
+        int length=0;
+        while(j<middle->str_len){ //middle.strから探す
+            //printf("%d %c %s\n",j,middle->str[j],middle->parts[partlength]);
+            if((middle->str[j]==middle->parts[partlength][0]) || (middle->str[j]=='x')){//もし一文字めが一緒であれば
+                for(i=0;i<strlen(middle->parts[partlength]);i++){
+                    if((middle->str[j+i]!=middle->parts[partlength][i]) && (middle->str[j+i]!='x')){//異なる場所があったらループを抜ける
+                    //printf("%c %c %d %lu\n",middle->str[j+i],middle->parts[partlength][i],i,strlen(middle->parts[partlength]));
                         break;
                     }
                 }
-                if(k==wordlength && flag==0){
-                    flag=1;//見つけてることを示す
-                    memo=j;
-                }else if(flag==1){
-                    flag=0;//もう見つけてるなら不確定
-                    break;//探す必要がないので飛ばす
+                if((i==(int)strlen(middle->parts[partlength])) && (flag==0)){
+                    flag=1;//見つけたフラグを立てる
+                    memostr=j;//場所のメモ
+                    //printf("%d\n",memostr);
+                }else if(i==(int)strlen(middle->parts[partlength])){
+                    flag=0;//悩ましいので次に回す
                 }
+                //printf("%d\n",flag);
             }
-            if(flag==1){//見つけられたのなら確定
-                for(j=wordlength;j<middle->strLen;j++){
-                    middle->str[j]=middle->unsettledPlace[j];//決まったものを文字列に入れる
-                }
-                middle->partsNum[wordlength]--;//一つ減らす
-                if(wordlength==middle->maxPartsLen){//maxが変わるのなら修正
-                    int maxlength=wordlength-1;
-                    for(l=maxlength-1;l>0;l--){
-                        if(middle->partsNum[l]!=0){
-                            middle->maxPartsLen=l;
-                            break;
-                        }
-                    }
-                }
-                if(memo!=middle->partsNum[wordlength]){//当てはまらない文字列の情報修正
-                    for(j=memo;j<middle->partsNum[wordlength]-1;j++){
-                        middle->parts[wordlength][j]=middle->parts[wordlength][j+1];
-                    }
-                }
-                k=0;
-                while(middle->unsettledPlace[k+i]!=0){//xの場所のリセット
-                    middle->unsettledPlace[k+i]=middle->unsettledPlace[k+i+1];
-                    k++;
-                }
-            }
+            j++;//次に行く
         }
-        i++;//次のものを探す
+        j=0;//jのリセット
+        if(flag==1){//もし1つだけ見つけられているのであれば、この部分は確定であることから決定してしまう
+            int k=0;
+            for(k=0;k<strlen(middle->parts[partlength]);k++){
+                //printf("%c %c \n",middle->str[k+memostr],middle->parts[partlength][k]);
+                middle->str[k+memostr]=middle->parts[partlength][k];//strにpartを写す
+                //printf("%c %c \n",middle->str[k+memostr],middle->parts[partlength][k]);
+            }
+            for(k=partlength;k<middle->partsNum-1;k++){//決まったpartを消す
+                //printf("%s %s\n",middle->parts[k],middle->parts[k+1]);
+                middle->parts[k]=middle->parts[k+1];
+                //printf("%s %s\n",middle->parts[k],middle->parts[k+1]);
+            }
+            cnt++;
+            flag=0;//flagのリセット
+            //printf("%s\n",middle->str);
+        }
+        partlength++;//次に行く
     }
+    middle->partsNum-=cnt;
     return *middle;
 }
