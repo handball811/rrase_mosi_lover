f58708008e9e58c4044aa7ba546044c4986daea4
鈴木　嘉恵 <>
2019-12-19 15:55:01 +0900

    no message

--- Solver/PredictionSolver/Prediction1/test.c
+++ Solver/PredictionSolver/Prediction1/test.c
@@ -7,16 +7,22 @@ int main(){
     middle_predict_structure middletest;
 
     strcpy(middletest.str, "abcdabcdabcdxxcd");
-    middletest.str_len = 16;
+    middletest.strLen = 16;
+    middletest.unsettledPlace[0]=0;
+    middletest.unsettledPlace[1]=3;
+    middletest.unsettledPlace[2]=12;
+    middletest.unsettledPlace[3]=16;
 
-    middletest.parts = (char **)malloc(sizeof(char *) * 2);
-	for(int i=0;i<2;i++){
-		middletest.parts[i] = (char *)malloc(sizeof(char) * 10);
+    middletest.parts = (char *)malloc(sizeof(char *) * 2);
+	    for(int i=0;i<2;i++){
+		    middletest.parts[i] = (char *)malloc(sizeof(char) * 10);
 	}
 
-    middletest.parts[0]="abc";
-    middletest.parts[1]="cccd";
-    middletest.partsNum = 2;
+    middletest.parts[3][0]="abc";
+    middletest.parts[4][0]="cccd";
+    middletest.partsNum[3] = 1;
+    middletest.partsNum[4] = 1;
+    middletest.maxPartsLen=4;
 
     printf("%s\n",middletest.str);
 
